cc1f14f5ac985f4b731a7a362120dd6823aef6ce
huber <huber@fim.uni-passau.de>
2022-05-20 11:03:52 +0200

    Added things

--- uebung1/boxes.cpp
+++ uebung1/boxes.cpp
@@ -4,41 +4,46 @@
 
 using namespace std;
 
-vector<int> boxes;
-vector<bool> done;
+
 
 int main() {
     int boxCount;
     cin >> boxCount;
 
-    done.resize(boxCount);
+    vector<int> boxes(boxCount);
+    vector<bool> done(boxCount);
+    vector<bool> belegt(boxCount);
+
+
 
     for(int i = 0; i<boxCount; ++i) {
-        int x = 0;
-        cin >> x;
-        boxes.push_back(x);
+        cin >> boxes[i];
         done[i] = false;
+        belegt[i] = false;
     }
+
     sort(boxes.begin(), boxes.end());
 
     int l = 0;
-    int res = 1;
-    while(l<boxCount-1) {
+    int res = 0;
+    while(l<boxCount) {
         for(int i = l+1; i<boxCount; ++i) {
-            if(boxes[l]*2<=boxes[i] && !done[i]) {
-                done[i] = true;
-                l = i;
+            if(boxes[l]*2<=boxes[i] && !belegt[i] && !done[i]) {
+                done[l] = true;
+                belegt[i] = true;
                 //res +=1;
                 break;
             }
         }
+        l += 1;
     }
-    for(int i = 0; i<boxCount; ++i) {
-        if(done[i]) {
+
+    for(int i = 0; i< boxCount; ++i) {
+        if(!done[i]) {
             res += 1;
         }
     }
 
-    cout << res;
+    cout << res << "\n";
 
 }
