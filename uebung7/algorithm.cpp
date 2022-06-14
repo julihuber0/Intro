7c9dc96cc606505562f3a1132901661f9b52a5ea
julihuber0 <julianhuber162@gmail.com>
2022-06-14 22:58:43 +0200

    Added things.

--- uebung7/algorithm.cpp
+++ uebung7/algorithm.cpp
@@ -25,4 +25,76 @@ vector<int> dijkstra(vector<vector<pi>> &list, int s) {
     return d;
 }
 
-int main(){}
+int find(int x, vector<int> &parent) {
+    if(parent[x] == -1) {
+        return x;
+    }
+    parent[x] = find(parent[x], parent);
+    return parent[x];
+}
+
+bool makeUnion(int a, int b, vector<int> &parent, vector<int> &rank) {
+    int x = find(a, parent);
+    int y = find(b, parent);
+    if(x!=y) {
+        parent[x] = y;
+    }
+    if(rank[x] > rank[y]) {
+        parent[y] = x;
+    } else if(rank[x] < rank[y]) {
+        parent[x] = y;
+    } else {
+        parent[x] = y;
+        rank[y] = rank[y] + 1;
+    }
+    return true;
+}
+
+bool isSame(int x, int y, vector<int> &parent) {
+    return find(x, parent) == find(y, parent);
+}
+
+struct wEdge{
+    int x;
+    int y;
+    int w;
+};
+
+bool kleiner(const wEdge &w1, const wEdge &w2) {
+    if(w1.w<w2.w) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool mst(vector<wEdge> &edges, int n) {
+    vector<int> parent(n, -1);
+    vector<int> rank(n, 0);
+    int mst_weight = 0;
+    for(wEdge w:edges) {
+        if(!isSame(w.x, w.y, parent)) {
+            makeUnion(w.x, w.y, parent, rank);
+            mst_weight += w.w;
+        }
+    }
+}
+
+int main(){
+    vector<int> parent(4, -1);
+    vector<int> rank(4, 0);
+    makeUnion(0, 1, parent, rank);
+    makeUnion(1, 2, parent, rank);
+    bool qu = isSame(0, 2, parent);
+    if(qu) {
+        cout << "Yes" << "\n";
+    } else {
+        cout << "No" << "\n";
+    }
+    qu = isSame(1, 3, parent);
+    if(qu) {
+        cout << "Yes" << "\n";
+    } else {
+        cout << "No" << "\n";
+    }
+}
